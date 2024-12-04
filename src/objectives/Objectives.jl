77201bb2d618e7e050a9e98d74c4b7f69c7854f1
Craig M. Hamel <cmhamel32@gmail.com>
2024-12-04 13:56:06 -0500

    moving around objectives into there own folder.

--- src/objectives/Objectives.jl
+++ src/objectives/Objectives.jl
@@ -75,94 +75,95 @@ Abstract base type for objective function parameters.
 """
 abstract type AbstractObjectiveParameters end
 
-"""
-$(TYPEDEF)
-$(TYPEDFIELDS)
-Type for objective function parameters for design parameters
-such as coordinates, time, bc values, properties
-state variables, and some scratch arrays.
-"""
-struct ObjectiveParameters{U1, T, B, N, S, P, U2, U3, V1, U4, Q} <: AbstractObjectiveParameters
-# struct ObjectiveParameters{U1, T, B, N, S, P, U2, U4, Q} <: AbstractObjectiveParameters
-  # design parameters
-  X::U1
-  t::T
-  Ubc::B # dirichlet bc design parameters
-  nbc::N # neumann bc design parameters
-  state_old::S
-  state_new::S
-  props::P
-  # scratch arrays
-  U::U2
-  grad_scratch::U3
-  grad_vec_scratch::V1
-  hvp_scratch::U4
-  q_vals_scratch::Q
-end
+# """
+# $(TYPEDEF)
+# $(TYPEDFIELDS)
+# Type for objective function parameters for design parameters
+# such as coordinates, time, bc values, properties
+# state variables, and some scratch arrays.
+# """
+# struct ObjectiveParameters{U1, T, B, N, S, P, U2, U3, V1, U4, Q} <: AbstractObjectiveParameters
+# # struct ObjectiveParameters{U1, T, B, N, S, P, U2, U4, Q} <: AbstractObjectiveParameters
+#   # design parameters
+#   X::U1
+#   t::T
+#   Ubc::B # dirichlet bc design parameters
+#   nbc::N # neumann bc design parameters
+#   state_old::S
+#   state_new::S
+#   props::P
+#   # scratch arrays
+#   U::U2
+#   grad_scratch::U3
+#   grad_vec_scratch::V1
+#   hvp_scratch::U4
+#   q_vals_scratch::Q
+# end
 
-"""
-$(TYPEDSIGNATURES)
-Constructor for a ```ObjectiveParameters``` type.
-```o``` - Objective function object.
-```times``` - Times object.
-"""
-function ObjectiveParameters(o::Objective, times)
-  X = copy(o.domain.coords)
-  U = create_fields(o.domain)
-  # boundary conditions
-  Ubc = Vector{eltype(X)}(undef, 0)
-  nbc = Vector{SVector{size(X, 1), eltype(X)}}(undef, 0)
-  update_dirichlet_vals!(Ubc, o.domain, X, times)
-  update_neumann_vals!(nbc, o.domain, X, times)
-  # properties
-  props = map(sec -> sec.props, o.domain.sections)
-  props = ComponentArray(props)
-  # scratch arrays
-  grad_scratch = create_fields(o.domain)
-  grad_vec_scratch = create_unknowns(o.domain)
-  hvp_scratch = create_fields(o.domain)
-  # need a scratch array for calculating q values on gpus
-  # TODO move somewhere else
-  q_vals_scratch = Dict{Symbol, Any}()
-  state_old = Dict{Symbol, Any}()
-  state_new = Dict{Symbol, Any}()
-  for (name, sec) in pairs(o.domain.sections)
-    NQ = FiniteElementContainers.num_q_points(sec.fspace)
-    NE = FiniteElementContainers.num_elements(sec.fspace)
-    q_vals_scratch[name] = zeros(eltype(X), NQ, NE)
-    state_old[name] = repeat(init_state(sec.physics), outer=(1, NQ, NE))
-    state_new[name] = repeat(init_state(sec.physics), outer=(1, NQ, NE))
-  end
-  q_vals_scratch = ComponentArray(q_vals_scratch)
-  state_old = ComponentArray(state_old)
-  state_new = ComponentArray(state_new)
-  params = ObjectiveParameters(
-    X, times, Ubc, nbc, state_old, state_new, props,
-    U, grad_scratch, grad_vec_scratch, hvp_scratch, q_vals_scratch
-    # U, hvp_scratch, q_vals_scratch
-  )
-  return params
-end
+# """
+# $(TYPEDSIGNATURES)
+# Constructor for a ```ObjectiveParameters``` type.
+# ```o``` - Objective function object.
+# ```times``` - Times object.
+# """
+# function ObjectiveParameters(o::Objective, times)
+#   X = copy(o.domain.coords)
+#   U = create_fields(o.domain)
+#   # boundary conditions
+#   Ubc = Vector{eltype(X)}(undef, 0)
+#   nbc = Vector{SVector{size(X, 1), eltype(X)}}(undef, 0)
+#   update_dirichlet_vals!(Ubc, o.domain, X, times)
+#   update_neumann_vals!(nbc, o.domain, X, times)
+#   # properties
+#   props = map(sec -> sec.props, o.domain.sections)
+#   props = ComponentArray(props)
+#   # scratch arrays
+#   grad_scratch = create_fields(o.domain)
+#   grad_vec_scratch = create_unknowns(o.domain)
+#   hvp_scratch = create_fields(o.domain)
+#   # need a scratch array for calculating q values on gpus
+#   # TODO move somewhere else
+#   q_vals_scratch = Dict{Symbol, Any}()
+#   state_old = Dict{Symbol, Any}()
+#   state_new = Dict{Symbol, Any}()
+#   for (name, sec) in pairs(o.domain.sections)
+#     NQ = FiniteElementContainers.num_q_points(sec.fspace)
+#     NE = FiniteElementContainers.num_elements(sec.fspace)
+#     q_vals_scratch[name] = zeros(eltype(X), NQ, NE)
+#     state_old[name] = repeat(init_state(sec.physics), outer=(1, NQ, NE))
+#     state_new[name] = repeat(init_state(sec.physics), outer=(1, NQ, NE))
+#   end
+#   q_vals_scratch = ComponentArray(q_vals_scratch)
+#   state_old = ComponentArray(state_old)
+#   state_new = ComponentArray(state_new)
+#   params = ObjectiveParameters(
+#     X, times, Ubc, nbc, state_old, state_new, props,
+#     U, grad_scratch, grad_vec_scratch, hvp_scratch, q_vals_scratch
+#     # U, hvp_scratch, q_vals_scratch
+#   )
+#   return params
+# end
 
-function zero_parameters!(p::ObjectiveParameters)
-  p.X .= zero(eltype(p.X))
-  # p.t .= 
-  if length(p.Ubc) > 0
-    p.Ubc .= zero(eltype(p.Ubc))
-  end
-  if length(p.nbc) > 0
-    for n in axes(p.nbc, 1)
-      p.nbc[n] = zero(typeof(p.nbc[n]))
-    end
-  end
-  p.state_old .= zero(eltype(p.state_old))
-  p.state_new .= zero(eltype(p.state_new))
-  p.props .= zero(eltype(p.props))
-  p.U .= zero(eltype(p.U))
-  p.hvp_scratch .= zero(eltype(p.hvp_scratch))
-  p.q_vals_scratch .= zero(eltype(p.q_vals_scratch))
-  return nothing
-end
+# function zero_parameters!(p::ObjectiveParameters)
+#   p.X .= zero(eltype(p.X))
+#   # p.t .= 
+#   if length(p.Ubc) > 0
+#     p.Ubc .= zero(eltype(p.Ubc))
+#   end
+#   if length(p.nbc) > 0
+#     for n in axes(p.nbc, 1)
+#       p.nbc[n] = zero(typeof(p.nbc[n]))
+#     end
+#   end
+#   p.state_old .= zero(eltype(p.state_old))
+#   p.state_new .= zero(eltype(p.state_new))
+#   p.props .= zero(eltype(p.props))
+#   p.U .= zero(eltype(p.U))
+#   p.hvp_scratch .= zero(eltype(p.hvp_scratch))
+#   p.q_vals_scratch .= zero(eltype(p.q_vals_scratch))
+#   return nothing
+# end
+include("ObjectiveParameters.jl")
 
 function gradient(o::Objective, Uu, p)
   @timeit timer(o) "Objective - gradient" begin
---
+++ src/objectives/UnconstrainedObjective.jl
@@ -0,0 +1,60 @@
+"""
+$(TYPEDEF)
+$(TYPEDFIELDS)
+Objective type for evaluating objective functions.
+The functions correspond to quadrature level evaluations
+of the objective function, it's gradient, and it's hessian
+respectively.
+```domain``` - A domain object 
+```value``` - A function for the quadrature level objective function evaluation.
+```gradient``` - A function for the quadrature level objective function gradient evaluation.
+```hessian``` - A function for the quadrature level objective hessian evaluation.
+```timer``` - A timer that's already setup.
+"""
+struct UnconstrainedObjective{
+  D, 
+  F1, F2, F3, 
+  F4, F5, F6, 
+  T
+} <: AbstractObjective
+  domain::D
+  value::F1
+  gradient::F2
+  hessian::F3
+  neumann_value::F4
+  neumann_gradient::F5
+  neumann_hessian::F6
+  timer::T
+end
+
+function UnconstrainedObjective(domain::Domain, value_func::F1, neumann_value_func::F2, timer::TimerOutput) where {F1 <: Function, F2 <: Function}
+  grad_func(phys, cell, u, x, state, props, t) = ForwardDiff.gradient(z -> value_func(phys, cell, z, x, state, props, t), u)
+  hess_func(phys, cell, u, x, state, props, t) = ForwardDiff.hessian(z -> value_func(phys, cell, z, x, state, props, t), u)
+  neumann_grad_func(phys, cell, u, x, t, bc, val, r, q, f) = ForwardDiff.gradient(z -> neumann_value_func(phys, cell, z, x, t, bc, val, r, q, f), u)
+  neumann_hess_func(phys, cell, u, x, t, bc, val, r, q, f) = ForwardDiff.hessian(z -> neumann_value_func(phys, cell, z, x, t, bc, val, r, q, f), u)
+  return UnconstrainedObjective(
+    domain, 
+    value_func, grad_func, hess_func, 
+    neumann_value_func, neumann_grad_func, neumann_hess_func,
+    timer
+  )
+end
+
+function UnconstrainedObjective(domain::Domain, ::typeof(energy))
+  return UnconstrainedObjective(
+    domain,
+    energy, gradient, hessian,
+    neumann_energy, neumann_gradient, neumann_hessian,
+    TimerOutput()
+  )
+end
+
+function UnconstrainedObjective(inputs::Dict{Symbol, Any}, domain, timer)
+  value_func = eval(Meta.parse(inputs[:value]))
+
+  if typeof(value_func) == typeof(energy)
+    return UnconstrainedObjective(domain, value_func)
+  else
+    throw(ErrorException("Unsupported objective function"))
+  end
+end
