74e14da1753792fc249c2f0d3488dadaf925c57f
Craig M. Hamel <cmhamel32@gmail.com>
2025-10-22 10:05:22 -0400

    removing constraints for now

--- src/objectives/ConstrainedObjective.jl
+++ src/objectives/ConstrainedObjective.jl
@@ -1,24 +1,24 @@
-abstract type AbstractConstraint{F <: Function} end
-abstract type AbstractConstraintCache{
-    O <: AbstractObjectiveCache
-} end
+# abstract type AbstractConstraint{F <: Function} end
+# abstract type AbstractConstraintCache{
+#     O <: AbstractObjectiveCache
+# } end
 
-struct ConstrainedObjective{
-    O <: AbstractObjective,
-    C <: AbstractConstraint
-}
-    objective::O
-    constraint::C
-end
+# struct ConstrainedObjective{
+#     O <: AbstractObjective,
+#     C <: AbstractConstraint
+# }
+#     objective::O
+#     constraint::C
+# end
 
-struct ConstrainedObjectiveCache{
-    C <: AbstractConstraint
-    O <: AbstractObjectiveCache
-} <: AbstractConstraintCache{O}
-    constraint::C
-    objective_cache::O
-end
+# struct ConstrainedObjectiveCache{
+#     C <: AbstractConstraint
+#     O <: AbstractObjectiveCache
+# } <: AbstractConstraintCache{O}
+#     constraint::C
+#     objective_cache::O
+# end
 
-function ConstrainedObjectiveCache(sim)
+# function ConstrainedObjectiveCache(sim)
 
-end
+# end
